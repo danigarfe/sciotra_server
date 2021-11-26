e3241f2fac74502ee0954c032ab680238419d523
Daniel García <danigarfe@gmail.com>
2021-11-26 10:33:58 +0100

    bbdd working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+var mysql      = require('mysql');
 const app = express()
 const port = 3000
+var connection = mysql.createConnection({
+    user     : 'root',
+    password: 'root',
+    host     : 'localhost',
+    database: 'bbdd'
+});
+connection.connect();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
