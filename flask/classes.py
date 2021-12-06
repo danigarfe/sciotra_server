028be77172ba5685ca69f1f4617aa555ccf62d6d
DESKTOP-FI3IHD8\tobeit <jonatanrm35@gmail.com>
2021-12-06 22:49:57 +0100

    comentarios_Jona

--- flask/classes.py
+++ flask/classes.py
@@ -5,21 +5,25 @@ from functions import *
 #CADA TIPUS DE THREAD ÉS UNA CLASS
 class autobus_bus:
     #N => MULTIPLICADOR DE TEMPS DEFINIT AL DOCUMENT DE CRITERIS DE SIMULACIÓ
-    #id => ID DEL SENSOR EN CONCRET
+    #id_bus => ID DEL SENSOR EN CONCRET
     #lin => LÍNEA D'AUTOBUS DEL SENSOR
-    def __init__(self, N, id, lin):
+    def __init__(self, N, id_bus, lin, parades):
         self.interval = (5/N) * 60 #interval d'execució en segons
         self.linia = lin
-        self.id=id
+        self.id_bus=id_bus
+        self.parades=parades
     #S'EXECUTA AL THREAD, BUCLE INFINIT AMB PARADES sleep() 
     def run(self):
-        #PARADAS DEFINIDAS POR ORDEN DE APARICIÓN. HACIA LA ROTONDA DE LA PARADA 32 A LA 28, Y VUELTA DE LA PARADA 27 A LA 29.
+        #PARADAS DEFINIDAS POR ORDEN DE APARICIÓN. (HACIA LA ROTONDA de Francesc Macià) DE LA PARADA 32 A LA 28, Y VUELTA DE LA (ROTONDA de Francesc Macià hacía ZONA UNIVERSITARÍA) PARADA 27 A LA 29.
+        #Este número de paradas se refiere a las establecidas en Google Earth. No a la de los autobuses.
         #SIMULA UN BUS QUE VA DANDO VUELTAS CONSTANTEMENTE AL TRAMO DE DIAGONAL SELECCIONADO
-        parades = [32, 30, 2, 33, 5, 6, 7, 9, 13, 11, 15, 17, 18, 19, 21, 22, 26, 28, 27, 25, 24, 23, 20, 16, 14, 12, 10, 8, 4, 3, 1, 31, 29]
+        #parades = [32, 30, 2, 33, 5, 6, 7, 9, 13, 11, 15, 17, 18, 19, 21, 22, 26, 28, 27, 25, 24, 23, 20, 16, 14, 12, 10, 8, 4, 3, 1, 31, 29]
         count = 0
+        #Aquí la clave sería ir guardando las llegadas de los buses a cada parada en especifico en la base de datos. Primero lo dejamos así y luego podemos subir el tiempo de simulación para no cargar tanto de datos la BBDD.
         while True:
             sleep(self.interval)
-            print("HA LLEGADO EL BUS ID=" + str(self.id) + " DE LA LÍNEA " + self.linia + " A LA PARADA " + str(parades[count%len(parades)]))
+            print("HA LLEGADO EL BUS ID=" + str(self.id_bus) + " DE LA LÍNEA " + self.linia + " A LA PARADA " + str(self.parades[count%len(self.parades)]))
             count = count+1
+            #Aquí hacer un insert de Autobus_bus.
             
 
--- flask/functions.py
+++ flask/functions.py
@@ -1,6 +1,9 @@
 from classes import *
 
 #OBTÉ LA DARRERA ENTRADA DE LA BASE DE DADES DE LA TAULA ESPECIFICADA
+#Lo mejor seria coger los últimos 5 buses que han llegado a esa parada especifica a través del timestamp
+#autobus_bus es un registro del bus que ha pasado por la parada con ID_parada.
+#La clave seria buscar por la ID_Parada y por el últimos timestamp de todas las lineas.
 def getlast(table, mysql):
     q = "SELECT * FROM " + table + " ORDER BY id DESC LIMIT 1;"
     cur = mysql.connection.cursor()
@@ -23,7 +26,11 @@ def insert(table, values, mysql):
 
 # LA IDEA ÉS FER UN THREAD PER A CADA "SENSOR VIRTUAL". COMPORTAMENT DE CADA TIPUS DE SENSOR DEFINIT A classes.py
 def initsensors(mysql):
-    bus = autobus_bus(30, 0, 'L69')
+    #hacer array de buses bus[i], recorrer un array de lineas de bus ('L69', 'L70'...) Runearlos TODOS de una.
+    #El 0 representa el id_bus. Tiene que ser valor único.
+    #autobus_bus(N, id_bus, lin, parades)
+    bus = autobus_bus(30, 0, 'L69', [32, 30, 2, 33, 5, 6, 7, 9, 13, 11, 15, 17, 18,
+                      19, 21, 22, 26, 28, 27, 25, 24, 23, 20, 16, 14, 12, 10, 8, 4, 3, 1, 31, 29])
     t = Thread( target=bus.run )
     t.start()
     return 0
